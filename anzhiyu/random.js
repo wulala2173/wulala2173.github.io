07f8dc46974dbafd16be27b1fbbcfbc5a12d3979
wulala2173 <wulala2173@outlook.com>
2024-01-18 23:38:53 +0800

    Site updated: 2024-01-18 23:38:52

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/01/08/2023-12-12-blog/","2024/01/03/hello-world/"];function toRandomPost(){
+var posts=["2024/01/03/hello-world/","2024/01/08/2023-12-12-blog/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
