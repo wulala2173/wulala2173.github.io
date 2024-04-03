ca1454be2ec1643e60330a723ba0ce15dcf67866
wulala2173 <wulala2173@outlook.com>
2024-04-03 10:35:40 +0800

    Site updated: 2024-04-03 10:35:38

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/01/08/2023-12-12-blog/","2024/01/30/2023-12-27/","2024/01/03/hello-world/","2024/03/15/jiao-zhao/","2023/12/30/log/","2024/01/09/some-linear-algebra/","2024/01/30/xie-algebra/"];function toRandomPost(){
+var posts=["2024/01/08/2023-12-12-blog/","2024/01/30/2023-12-27/","2024/01/03/hello-world/","2024/03/15/jiao-zhao/","2023/12/30/log/","2024/01/09/some-linear-algebra/","2024/01/30/xie-algebra/","2024/03/08/xin-gao-kao/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
