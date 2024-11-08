0f2759ef15c8af5b6186002734a4e8e64ef214f9
wulala2173 <wulawula2173@outlook.com>
2024-11-08 08:08:26 +0800

    Site updated: 2024-11-08 08:08:25

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/01/08/2023-12-12-blog/","2024/01/30/2023-12-27/","2024/04/11/film-review/","2024/10/31/hello-world/","2023/12/30/log/","2024/03/15/jiao-zhao/","2024/03/15/shifenxi-t/","2024/04/30/some feel/","2024/01/09/some-linear-algebra/","2024/01/30/xie-algebra/","2024/03/08/xin-gao-kao/"];function toRandomPost(){
+var posts=["2024/01/08/2023-12-12-blog/","2024/01/30/2023-12-27/","2024/04/11/film-review/","2024/10/31/hello-world/","2023/12/30/log/","2024/03/15/jiao-zhao/","2024/03/15/shifenxi-t/","2024/04/30/some feel/","2024/01/09/some-linear-algebra/","2024/01/30/xie-algebra/","2024/03/08/xin-gao-kao/","2024/09/08/learnfortest/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
